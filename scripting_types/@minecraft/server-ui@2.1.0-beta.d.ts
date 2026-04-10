f516ac360ae9a0f73d9184b07095f215a9fa4e02
King <47607185+xKingDark@users.noreply.github.com>
2026-04-10 17:29:22 +0100

    More UI type definitions

--- scripting_types/@minecraft/server-ui@2.1.0-beta.d.ts
+++ scripting_types/@minecraft/server-ui@2.1.0-beta.d.ts
@@ -22,6 +22,18 @@ export enum FormRejectReason {
     ServerShutdown    = "ServerShutdown",
 }
 
+export declare enum DataDrivenScreenClosedReason {
+    ServerClose = "ServerClose",
+    UserBusy    = "UserBusy",
+    UserClose   = "UserClose",
+}
+
+export declare enum TextFilteringError {
+    DisabledByPlayer                = "DisabledByPlayer",
+    TextProcessorServiceUnreachable = "TextProcessorServiceUnreachable",
+    Unknown                         = "Unknown",
+}
+
 export class ActionFormData {
     body(bodyText: minecraftserver.RawMessage | string): ActionFormData;
     button(text: minecraftserver.RawMessage | string, iconPath?: string): ActionFormData;
@@ -133,6 +145,93 @@ export class UIManager {
     closeAllForms(player: minecraftserver.Player): void;
 }
 
+export declare class CustomForm {
+    button(
+        label: Observable<string> | Observable<UIRawMessage> | string | UIRawMessage,
+        onClick: () => void,
+        buttonOptions?: ButtonOptions,
+    ): CustomForm;
+    close(): void;
+    closeButton(): CustomForm;
+    divider(dividerOptions?: DividerOptions): CustomForm;
+    dropdown(
+        label: Observable<string> | Observable<UIRawMessage> | string | UIRawMessage,
+        arg1: Observable<number>,
+        items: DropdownItem[],
+        dropdownOptions?: DropdownOptions,
+    ): CustomForm;
+    header(
+        label: Observable<string> | Observable<UIRawMessage> | string | UIRawMessage,
+        textOptions?: TextOptions,
+    ): CustomForm;
+    isShowing(): boolean;
+    label(
+        label: Observable<string> | Observable<UIRawMessage> | string | UIRawMessage,
+        textOptions?: TextOptions,
+    ): CustomForm;
+    /**
+     * @remarks This function can't be called in restricted-execution mode.
+     */
+    show(): Promise<DataDrivenScreenClosedReason>;
+    slider(
+        label: Observable<string> | Observable<UIRawMessage> | string | UIRawMessage,
+        arg1: Observable<number>,
+        arg2: Observable<number> | number,
+        arg3: Observable<number> | number,
+        sliderOptions?: SliderOptions,
+    ): CustomForm;
+    spacer(spacingOptions?: SpacingOptions): CustomForm;
+    textField(
+        label: Observable<string> | Observable<UIRawMessage> | string | UIRawMessage,
+        arg1: Observable<string>,
+        textFieldOptions?: TextFieldOptions,
+    ): CustomForm;
+    toggle(
+        label: Observable<string> | Observable<UIRawMessage> | string | UIRawMessage,
+        arg1: Observable<boolean>,
+        toggleOptions?: ToggleOptions,
+    ): CustomForm;
+    static create(
+        player: minecraftserver.Player,
+        tooltip: Observable<string> | Observable<UIRawMessage> | string | UIRawMessage,
+    ): CustomForm;
+}
+
+export declare class MessageBox {
+    body(bodyText: Observable<string> | Observable<UIRawMessage> | string | UIRawMessage): MessageBox;
+    button1(
+        label: Observable<string> | Observable<UIRawMessage> | string | UIRawMessage,
+        arg1?: Observable<string> | Observable<UIRawMessage> | string | UIRawMessage,
+    ): MessageBox;
+    button2(
+        label: Observable<string> | Observable<UIRawMessage> | string | UIRawMessage,
+        arg1?: Observable<string> | Observable<UIRawMessage> | string | UIRawMessage,
+    ): MessageBox;
+    close(): void;
+    isShowing(): boolean;
+    /**
+     * @remarks This function can't be called in restricted-execution mode.
+     */
+    show(): Promise<MessageBoxResult>;
+    static create(
+        player: minecraftserver.Player,
+        tooltip: Observable<string> | Observable<UIRawMessage> | string | UIRawMessage,
+    ): MessageBox;
+}
+
+export declare class Observable<T extends boolean | string | UIRawMessage | number> {
+    getData(): T;
+    getFilteredText(player: minecraftserver.Player): Promise<string | TextFilteringError[]>;
+    setData(arg0: T): void;
+    subscribe(callback: (arg0: T) => void): (arg0: T) => void;
+    toJSON(): unknown;
+    unsubscribe(callback: (arg0: T) => void): void;
+    static create<T extends boolean | string | UIRawMessage | number>(
+        arg0: T,
+        observableOptions?: ObservableOptions,
+    ): Observable<T>;
+}
+
 export interface ModalFormDataDropdownOptions {
     defaultValueIndex?: number;
     tooltip?: minecraftserver.RawMessage | string;
@@ -154,6 +253,71 @@ export interface ModalFormDataToggleOptions {
     tooltip?: minecraftserver.RawMessage | string;
 }
 
+export interface ButtonOptions {
+    disabled?: Observable<boolean> | boolean;
+    tooltip?: Observable<string> | Observable<UIRawMessage> | string | UIRawMessage;
+    visible?: Observable<boolean> | boolean;
+}
+
+export interface DividerOptions {
+    visible?: Observable<boolean> | boolean;
+}
+
+export interface DropdownItem {
+    description?: UIRawMessage | string;
+    label: UIRawMessage | string;
+    value: number;
+}
+
+export interface DropdownOptions {
+    description?: Observable<string> | string | UIRawMessage;
+    disabled?: Observable<boolean> | boolean;
+    visible?: Observable<boolean> | boolean;
+}
+
+export interface MessageBoxResult {
+    closeReason: DataDrivenScreenClosedReason;
+    selection?: number;
+}
+
+export interface ObservableOptions {
+    clientWritable?: boolean;
+}
+
+export interface SliderOptions {
+    description?: Observable<string> | Observable<UIRawMessage> | string | UIRawMessage;
+    disabled?: Observable<boolean> | boolean;
+    step?: Observable<number> | number;
+    visible?: Observable<boolean> | boolean;
+}
+
+export interface SpacingOptions {
+    visible?: Observable<boolean> | boolean;
+}
+
+export interface TextFieldOptions {
+    description?: Observable<string> | Observable<UIRawMessage> | string | UIRawMessage;
+    disabled?: Observable<boolean> | boolean;
+    visible?: Observable<boolean> | boolean;
+}
+
+export interface TextOptions {
+    visible?: Observable<boolean> | boolean;
+}
+
+export interface ToggleOptions {
+    description?: Observable<string> | Observable<UIRawMessage> | string | UIRawMessage;
+    disabled?: Observable<boolean> | boolean;
+    visible?: Observable<boolean> | boolean;
+}
+
+export interface UIRawMessage {
+    rawtext?: UIRawMessage[];
+    text?: string;
+    translate?: string;
+    with?: string[] | UIRawMessage;
+}
+
 // @ts-ignore
 export class FormRejectError extends Error {
     private constructor();
@@ -162,5 +326,14 @@ export class FormRejectError extends Error {
      */
     readonly reason: FormRejectReason;
 }
+
+// @ts-ignore
+export declare class FormCloseError extends Error {}
+
+// @ts-ignore
+export declare class PlayerLeftError extends Error {}
+
+// @ts-ignore
+export declare class ServerShutdownError extends Error {}
 
 export const uiManager: UIManager;
--- scripting_types/@minecraft/server-ui@2.1.0-internal.d.ts
+++ scripting_types/@minecraft/server-ui@2.1.0-internal.d.ts
@@ -41,6 +41,12 @@ export enum InternalTextFilteringError {
     Unknown                         = "Unknown",
 }
 
+export declare enum TextFilteringError {
+    DisabledByPlayer                = "DisabledByPlayer",
+    TextProcessorServiceUnreachable = "TextProcessorServiceUnreachable",
+    Unknown                         = "Unknown",
+}
+
 export class ActionFormData {
     body(bodyText: minecraftserver.RawMessage | string): ActionFormData;
     button(text: minecraftserver.RawMessage | string, iconPath?: string): ActionFormData;
@@ -360,6 +366,105 @@ export class DataStore {
     unsubscribe(onChange: (arg0: string) => void): boolean;
 }
 
+export declare class CustomForm {
+    button(
+        label: Observable<string> | Observable<UIRawMessage> | string | UIRawMessage,
+        onClick: () => void,
+        buttonOptions?: ButtonOptions,
+    ): CustomForm;
+    close(): void;
+    closeButton(): CustomForm;
+    divider(dividerOptions?: DividerOptions): CustomForm;
+    dropdown(
+        label: Observable<string> | Observable<UIRawMessage> | string | UIRawMessage,
+        arg1: Observable<number>,
+        items: DropdownItem[],
+        dropdownOptions?: DropdownOptions,
+    ): CustomForm;
+    header(
+        label: Observable<string> | Observable<UIRawMessage> | string | UIRawMessage,
+        textOptions?: TextOptions,
+    ): CustomForm;
+    isShowing(): boolean;
+    label(
+        label: Observable<string> | Observable<UIRawMessage> | string | UIRawMessage,
+        textOptions?: TextOptions,
+    ): CustomForm;
+    /**
+     * @remarks This function can't be called in restricted-execution mode.
+     */
+    show(): Promise<DataDrivenScreenClosedReason>;
+    slider(
+        label: Observable<string> | Observable<UIRawMessage> | string | UIRawMessage,
+        arg1: Observable<number>,
+        arg2: Observable<number> | number,
+        arg3: Observable<number> | number,
+        sliderOptions?: SliderOptions,
+    ): CustomForm;
+    spacer(spacingOptions?: SpacingOptions): CustomForm;
+    textField(
+        label: Observable<string> | Observable<UIRawMessage> | string | UIRawMessage,
+        arg1: Observable<string>,
+        textFieldOptions?: TextFieldOptions,
+    ): CustomForm;
+    toggle(
+        label: Observable<string> | Observable<UIRawMessage> | string | UIRawMessage,
+        arg1: Observable<boolean>,
+        toggleOptions?: ToggleOptions,
+    ): CustomForm;
+    static create(
+        player: minecraftserver.Player,
+        tooltip: Observable<string> | Observable<UIRawMessage> | string | UIRawMessage,
+    ): CustomForm;
+}
+
+export declare class MessageBox {
+    body(bodyText: Observable<string> | Observable<UIRawMessage> | string | UIRawMessage): MessageBox;
+    button1(
+        label: Observable<string> | Observable<UIRawMessage> | string | UIRawMessage,
+        arg1?: Observable<string> | Observable<UIRawMessage> | string | UIRawMessage,
+    ): MessageBox;
+    button2(
+        label: Observable<string> | Observable<UIRawMessage> | string | UIRawMessage,
+        arg1?: Observable<string> | Observable<UIRawMessage> | string | UIRawMessage,
+    ): MessageBox;
+    close(): void;
+    isShowing(): boolean;
+    /**
+     * @remarks This function can't be called in restricted-execution mode.
+     */
+    show(): Promise<MessageBoxResult>;
+    static create(
+        player: minecraftserver.Player,
+        tooltip: Observable<string> | Observable<UIRawMessage> | string | UIRawMessage,
+    ): MessageBox;
+}
+
+export declare class Observable<T extends boolean | string | UIRawMessage | number> {
+    bind(player: minecraftserver.Player, arg1: string, arg2: string, arg3: string): void;
+    getBindingCount(): number;
+    getData(): T;
+    getFilteredText(player: minecraftserver.Player): Promise<string | TextFilteringError[]>;
+    isClientWritable(): boolean;
+    setData(arg0: T): void;
+    subscribe(callback: (arg0: T) => void): (arg0: T) => void;
+    toJSON(): unknown;
+    unbind(player: minecraftserver.Player, arg1: string, arg2: string, arg3: string): void;
+    unsubscribe(callback: (arg0: T) => void): void;
+    waitForChange(): Promise<T>;
+    static create<T extends boolean | string | UIRawMessage | number>(
+        arg0: T,
+        observableOptions?: ObservableOptions,
+    ): Observable<T>;
+}
+
+export declare class ObservableCallback {
+    bind(player: minecraftserver.Player, arg1: string, arg2: string, arg3: string): void;
+    getData(): number;
+    unbind(player: minecraftserver.Player, arg1: string, arg2: string, arg3: string): void;
+    static create(onClick: () => void): ObservableCallback;
+}
+
 export interface ModalFormDataDropdownOptions {
     defaultValueIndex?: number;
     tooltip?: minecraftserver.RawMessage | string;
@@ -379,6 +484,71 @@ export interface ModalFormDataTextFieldOptions {
 export interface ModalFormDataToggleOptions {
     defaultValue?: boolean;
     tooltip?: minecraftserver.RawMessage | string;
+}
+
+export interface ButtonOptions {
+    disabled?: Observable<boolean> | boolean;
+    tooltip?: Observable<string> | Observable<UIRawMessage> | string | UIRawMessage;
+    visible?: Observable<boolean> | boolean;
+}
+
+export interface DividerOptions {
+    visible?: Observable<boolean> | boolean;
+}
+
+export interface DropdownItem {
+    description?: UIRawMessage | string;
+    label: UIRawMessage | string;
+    value: number;
+}
+
+export interface DropdownOptions {
+    description?: Observable<string> | string | UIRawMessage;
+    disabled?: Observable<boolean> | boolean;
+    visible?: Observable<boolean> | boolean;
+}
+
+export interface MessageBoxResult {
+    closeReason: DataDrivenScreenClosedReason;
+    selection?: number;
+}
+
+export interface ObservableOptions {
+    clientWritable?: boolean;
+}
+
+export interface SliderOptions {
+    description?: Observable<string> | Observable<UIRawMessage> | string | UIRawMessage;
+    disabled?: Observable<boolean> | boolean;
+    step?: Observable<number> | number;
+    visible?: Observable<boolean> | boolean;
+}
+
+export interface SpacingOptions {
+    visible?: Observable<boolean> | boolean;
+}
+
+export interface TextFieldOptions {
+    description?: Observable<string> | Observable<UIRawMessage> | string | UIRawMessage;
+    disabled?: Observable<boolean> | boolean;
+    visible?: Observable<boolean> | boolean;
+}
+
+export interface TextOptions {
+    visible?: Observable<boolean> | boolean;
+}
+
+export interface ToggleOptions {
+    description?: Observable<string> | Observable<UIRawMessage> | string | UIRawMessage;
+    disabled?: Observable<boolean> | boolean;
+    visible?: Observable<boolean> | boolean;
+}
+
+export interface UIRawMessage {
+    rawtext?: UIRawMessage[];
+    text?: string;
+    translate?: string;
+    with?: string[] | UIRawMessage;
 }
 
 // @ts-ignore
@@ -407,6 +577,18 @@ export class InvalidPathError extends Error {
      */
     readonly path: string;
 }
+
+// @ts-ignore
+export declare class FormCloseError extends Error {}
+
+// @ts-ignore
+export declare class InvalidFormError extends Error {}
+
+// @ts-ignore
+export declare class PlayerLeftError extends Error {}
+
+// @ts-ignore
+export declare class ServerShutdownError extends Error {}
 
 export const testConstant = 5;
 
